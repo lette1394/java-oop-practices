4b5255e8733071024259659137f3e089b723c2ca
lette1394 <lette1394@gmail.com>
2020-12-13 02:48:09 +0900

    Refactor variable

--- numberbaseball/src/main/java/com/github/lette1394/numberbaseball/DigitStream.java
+++ numberbaseball/src/main/java/com/github/lette1394/numberbaseball/DigitStream.java
@@ -7,25 +7,25 @@ public class DigitStream {
   private final int[] value;
 
   public int strikes(DigitStream other) {
-    int strike = 0;
+    int ret = 0;
     for (int i = 0; i < other.value.length; i++) {
       if (value[i] == other.value[i]) {
-        strike++;
+        ret++;
       }
     }
-    return strike;
+    return ret;
   }
 
   public int balls(DigitStream other) {
-    int balls = 0;
+    int ret = 0;
 
     for (int i = 0; i < other.value.length; i++) {
       for (int j = 0; j < other.value.length; j++) {
         if (value[i] == other.value[j] && i != j) {
-          balls++;
+          ret++;
         }
       }
     }
-    return balls;
+    return ret;
   }
 }
--- numberbaseball/src/main/java/com/github/lette1394/numberbaseball/Game.java
+++ numberbaseball/src/main/java/com/github/lette1394/numberbaseball/Game.java
@@ -3,12 +3,9 @@ package com.github.lette1394.numberbaseball;
 public class Game {
 
   public Result run(DigitStream com, DigitStream user) {
-    int strike = com.strikes(user);
-    int ball = com.balls(user);
-
     return Result.builder()
-      .ball(ball)
-      .strike(strike)
+      .strike(com.strikes(user))
+      .ball(com.balls(user))
       .build();
   }
 }

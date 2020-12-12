ea8ef7fd5a77c949439036c82dbb842adf896944
lette1394 <lette1394@gmail.com>
2020-12-13 02:22:58 +0900

    Rename Main, Add Game class, Move game loop logic

--- numberbaseball/src/test/java/com/github/lette1394/numberbaseball/MainTest.java
+++ numberbaseball/src/test/java/com/github/lette1394/numberbaseball/MainTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-class AppTest {
+class MainTest {
   @Test
   void test1() {
     System.out.println("hello world");

fa94298bb5b21825843993f1a6a724a92fd09cd0
lette1394 <lette1394@gmail.com>
2020-12-13 19:00:45 +0900

    Pass tests

--- calculator/src/main/java/com/github/lette1394/calculator/Calculator.java
+++ calculator/src/main/java/com/github/lette1394/calculator/Calculator.java
@@ -1,23 +1,39 @@
 package com.github.lette1394.calculator;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 
 public class Calculator {
   public long calculate(String expression) {
-    final String[] split = expression.split(" ");
+    final List<String> operators = Arrays
+      .stream(expression.split("[^+-]"))
+      .map(String::trim)
+      .filter(StringUtils::isNotBlank)
+      .collect(Collectors.toList());
 
-    final long a = Long.parseLong(split[0]);
-    final long b = Long.parseLong(split[2]);
-    final String operator = split[1];
+    final List<Long> operands = Arrays
+      .stream(expression.split("[+-]"))
+      .map(String::trim)
+      .map(Long::parseLong)
+      .collect(Collectors.toList());
 
-    if (operator.equals("+")) {
-      return a + b;
-    }
-
-    if (operator.equals("-")) {
-      return a - b;
-    }
-
-    return -1;
+    AtomicInteger index = new AtomicInteger(0);
+      return operands
+        .stream()
+        .reduce((a, b) -> {
+          final int i = index.getAndIncrement();
+          if (operators.get(i).equals("+")) {
+            return a + b;
+          }
+          if (operators.get(i).equals("-")) {
+            return a - b;
+          }
+          throw new RuntimeException();
+        })
+        .orElse(-1L);
   }
 }

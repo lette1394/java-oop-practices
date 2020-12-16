2fc2d6be669a7a963d2aa4e3747e8e7fb1f88b9a
lette1394 <lette1394@gmail.com>
2020-12-17 00:46:24 +0900

    Add failed test

--- calculator2/src/test/java/com/github/lette1394/calculator2/CalculatorTest.java
+++ calculator2/src/test/java/com/github/lette1394/calculator2/CalculatorTest.java
@@ -18,6 +18,7 @@ class CalculatorTest {
   void one_operand() {
     assertThat(subjectLong("0"), is(0L));
     assertThat(subjectLong("1"), is(1L));
+    assertThat(subjectLong("+1"), is(+1L));
     assertThat(subjectLong("-1"), is(-1L));
   }
 

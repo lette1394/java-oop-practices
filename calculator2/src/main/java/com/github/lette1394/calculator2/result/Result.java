b6bbb6e6ac44b5646ac123691e724f2f81fbb91f
lette1394 <lette1394@gmail.com>
2020-12-17 01:48:26 +0900

    Add TODO

--- calculator2/src/main/java/com/github/lette1394/calculator2/result/Result.java
+++ calculator2/src/main/java/com/github/lette1394/calculator2/result/Result.java
@@ -3,6 +3,10 @@ package com.github.lette1394.calculator2.result;
 public interface Result {
   long asLong();
 
+  // TODO: remove method
+  //  이거는 configurable 설정으로 풀어야 한다.
+  //  overflow/underflow를 허용 -> asLong() 호출 성공
+  //  overflow/underflow를 금지 -> Expression.evaluate() 에서 예외 발생
   long asLongExact();
 
   double asDouble();

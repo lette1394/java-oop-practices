4a80658369067f85dfff04bec8478e98ff71ba98
lette1394 <lette1394@gmail.com>
2020-12-17 00:01:08 +0900

    Refactor PreemptiveTimeoutExpressionTest

--- calculator2/src/test/java/com/github/lette1394/calculator2/expression/PreemptiveTimeoutExpressionTest.java
+++ calculator2/src/test/java/com/github/lette1394/calculator2/expression/PreemptiveTimeoutExpressionTest.java
@@ -1,23 +1,36 @@
 package com.github.lette1394.calculator2.expression;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import com.github.lette1394.calculator2.result.Result;
 import java.time.Duration;
 import org.junit.jupiter.api.Test;
 
 class PreemptiveTimeoutExpressionTest {
-  private static final Duration TIMEOUT = Duration.ofMillis(500);
+  private static final Duration TIMEOUT = Duration.ofMillis(100);
 
   @Test
   void timeout() {
-    final Expression expression = new DelayedExpression(
-      NumericExpressionFactory.INSTANCE.of("12345"), TIMEOUT);
-
-    assertThrows(EvaluationTimeoutException.class, () -> subject(expression));
+    assertThrows(EvaluationTimeoutException.class,
+      () -> subject(delayed("12345", TIMEOUT), TIMEOUT));
   }
 
-  private Result subject(Expression expression) {
-    return new PreemptiveTimeoutExpression(expression, TIMEOUT).evaluate();
+  @Test
+  void no_timeout() {
+    assertThat(subject(zeroDelayed("12345"), TIMEOUT), is(12345L));
+  }
+
+  private Expression zeroDelayed(String stringExpression) {
+    return new DelayedExpression(NumericExpressionFactory.INSTANCE.of(stringExpression),
+      Duration.ZERO);
+  }
+
+  private Expression delayed(String stringExpression, Duration timeout) {
+    return new DelayedExpression(NumericExpressionFactory.INSTANCE.of(stringExpression), timeout);
+  }
+
+  private long subject(Expression expression, Duration timeout) {
+    return new PreemptiveTimeoutExpression(expression, timeout).evaluate().asLongExact();
   }
 }

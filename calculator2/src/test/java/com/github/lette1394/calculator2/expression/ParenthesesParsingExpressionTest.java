9dd01b8f1f7d03913421cecac75783785c357786
lette1394 <lette1394@gmail.com>
2020-12-18 03:36:06 +0900

    Add failed test: double parentheses

--- calculator2/src/test/java/com/github/lette1394/calculator2/expression/ParenthesesParsingExpressionTest.java
+++ calculator2/src/test/java/com/github/lette1394/calculator2/expression/ParenthesesParsingExpressionTest.java
@@ -11,11 +11,14 @@ class ParenthesesParsingExpressionTest {
   @Test
   void parentheses() {
     assertThat(subjectLong("(1)"), is(1L));
+    assertThat(subjectLong("((1))"), is(1L));
     assertThat(subjectLong("(50)"), is(50L));
   }
 
   @Test
   void parentheses_two_operand() {
+    assertThat(subjectLong("(1) + 2"), is(3L));
+    assertThat(subjectLong("1 + (2)"), is(3L));
     assertThat(subjectLong("(1) + (2)"), is(3L));
   }
 

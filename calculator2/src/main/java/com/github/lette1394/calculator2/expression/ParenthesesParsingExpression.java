95258d539abd071432e8b05c46a4724a996e4b4a
lette1394 <lette1394@gmail.com>
2020-12-18 03:19:28 +0900

    Pass ParenthesesParsingExpressionTest

--- calculator2/src/main/java/com/github/lette1394/calculator2/expression/ParenthesesParsingExpression.java
+++ calculator2/src/main/java/com/github/lette1394/calculator2/expression/ParenthesesParsingExpression.java
@@ -1,22 +1,27 @@
 package com.github.lette1394.calculator2.expression;
 
-import com.github.lette1394.calculator2.operator.OperatorFinder;
 import com.github.lette1394.calculator2.result.Result;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParenthesesParsingExpression implements Expression {
   private final String expression;
+
+  // FIXME (jaeeun) 2020-12-18: 왠지 Matcher가지고 하나 클래스가 나올 거 같다...
   private final Matcher matcher;
   private final ExpressionFactory expressionFactory;
 
   public ParenthesesParsingExpression(
     String expression,
-    OperatorFinder operatorFinder,
     ExpressionFactory expressionFactory) {
     this.expression = expression;
-    this.matcher = Pattern.compile("(.+)").matcher(expression);
+    this.matcher = Pattern.compile("(.*)(\\(.+\\))(.*)").matcher(expression);
     this.expressionFactory = expressionFactory;
+  }
+
+  private static String unwrapParentheses(String value) {
+    final int length = value.length();
+    return value.substring(1, length - 1);
   }
 
   @Override
@@ -24,7 +29,22 @@ public class ParenthesesParsingExpression implements Expression {
                                   OverflowException,
                                   UnderflowException,
                                   EvaluationTimeoutException {
+    return parse(expression).evaluate();
+  }
 
-    return null;
+  private Expression parse(String expression) {
+    if (matcher.find()) {
+      final String left = matcher.group(1);
+      final Expression inner = expressionFactory.of(unwrapParentheses(matcher.group(2)));
+      final String right = matcher.group(3);
+      return parse(String.format("%s%s%s",
+        left, inner.evaluate().asString(), right));
+    }
+    return expressionFactory.of(expression);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
   }
 }
--- calculator2/src/test/java/com/github/lette1394/calculator2/expression/ParenthesesParsingExpressionTest.java
+++ calculator2/src/test/java/com/github/lette1394/calculator2/expression/ParenthesesParsingExpressionTest.java
@@ -1,15 +1,24 @@
 package com.github.lette1394.calculator2.expression;
 
-import static com.github.lette1394.calculator2.TestFixtures.subjectString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import com.github.lette1394.calculator2.result.Result;
 import org.junit.jupiter.api.Test;
 
 class ParenthesesParsingExpressionTest {
 
   @Test
   void parentheses() {
-    assertThat(subjectString("(1)"), is(1L));
+    assertThat(subjectLong("(1)"), is(1L));
+  }
+
+  private Long subjectLong(String expression) {
+    return subject(expression).asLongExact();
+  }
+
+  private Result subject(String expression) {
+    return new ParenthesesParsingExpression(expression, NumericExpressionFactory.INSTANCE)
+      .evaluate();
   }
 }

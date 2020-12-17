9ba30073e09fbbb2642b521810023c2de277b195
lette1394 <lette1394@gmail.com>
2020-12-18 02:58:42 +0900

    Add ParenthesesParsingExpression class

---
+++ calculator2/src/main/java/com/github/lette1394/calculator2/expression/ParenthesesParsingExpression.java
@@ -0,0 +1,16 @@
+package com.github.lette1394.calculator2.expression;
+
+import com.github.lette1394.calculator2.result.Result;
+
+public class ParenthesesParsingExpression implements Expression {
+
+
+  @Override
+  public Result evaluate() throws
+                           DivideByZeroException,
+                           OverflowException,
+                           UnderflowException,
+                           EvaluationTimeoutException {
+    return null;
+  }
+}

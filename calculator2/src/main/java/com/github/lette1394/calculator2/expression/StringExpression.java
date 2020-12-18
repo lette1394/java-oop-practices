e787288464e90cd41bd8602d8b4843f2cc56fdd6
lette1394 <lette1394@gmail.com>
2020-12-18 23:33:56 +0900

    ADd comments

--- calculator2/src/main/java/com/github/lette1394/calculator2/expression/StringExpression.java
+++ calculator2/src/main/java/com/github/lette1394/calculator2/expression/StringExpression.java
@@ -6,6 +6,10 @@ import static java.lang.Long.parseLong;
 import com.github.lette1394.calculator2.result.Result;
 import lombok.RequiredArgsConstructor;
 
+// FIXME (jaeeun) 2020-12-18: 이 클래스는 틀려먹었다.
+//  StringExpression 이 아니라 그냥 Long.
+//  StringExpression은 그 자체로 완전한 expression이 되어야 한다
+//  물론 ExpressionFactory에서 조합이 된 이후에
 @RequiredArgsConstructor
 class StringExpression implements Expression {
   private final String value;

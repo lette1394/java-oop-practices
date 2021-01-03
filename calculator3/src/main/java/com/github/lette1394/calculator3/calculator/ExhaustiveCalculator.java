73522de8e93cdd2f28e2564ddf34481fbb873c49
lette1394 <lette1394@gmail.com>
2021-01-03 18:26:28 +0900

    Add all test

--- calculator3/src/main/java/com/github/lette1394/calculator3/calculator/ExhaustiveCalculator.java
+++ calculator3/src/main/java/com/github/lette1394/calculator3/calculator/ExhaustiveCalculator.java
@@ -3,9 +3,12 @@ package com.github.lette1394.calculator3.calculator;
 import com.github.lette1394.calculator3.expression.AddExpression;
 import com.github.lette1394.calculator3.expression.AdderInNumeric;
 import com.github.lette1394.calculator3.expression.Expression;
+import java.util.regex.Pattern;
 
 public class ExhaustiveCalculator implements Calculator {
+  private static Pattern pattern = Pattern.compile("-?\\d+|-?\\d+\\.\\d+");
   private final Expression expression;
+
 
   public ExhaustiveCalculator() {
     expression = new AddExpression(new AdderInNumeric());
@@ -13,6 +16,10 @@ public class ExhaustiveCalculator implements Calculator {
 
   @Override
   public String evaluate(String expression) {
+    if (pattern.matcher(expression).matches()) {
+      return expression;
+    }
+
     return this.expression.evaluate(expression);
   }
 }

eca0197292800cdbc9710943513cb093c8e0015e
lette1394 <lette1394@gmail.com>
2021-01-03 17:53:03 +0900

    Add failed test

--- calculator3/src/test/groovy/CalculatorSpec.groovy
+++ calculator3/src/test/groovy/CalculatorSpec.groovy
@@ -3,18 +3,33 @@ import spock.lang.Specification
 
 
 class CalculatorSpec extends Specification {
-  def 'evaluate just a number #number == #expected'() {
+  def 'evaluate just a number #expression == #expected'() {
     given: 'a number'
     when: 'evaluate'
-    then: 'get a evaluated number'
-      evaluate(number) == expected
+    then: 'get a evaluated value'
+      evaluate(expression) == expected
     where:
-      number | expected
-      '123'  | '123'
-      '456'  | '456'
-      '3.14' | '3.14'
+      expression | expected
+      '123'      | '123'
+      '456'      | '456'
+      '3.14'     | '3.14'
+      '-500'     | '-500'
   }
 
+  def 'evaluate two operand expression #expression == #expected'() {
+    given: 'two operand expression'
+    when: 'evaluate'
+    then: 'get a evaluated value'
+      evaluate(expression) == expected
+    where:
+      expression | expected
+      '10+2'     | '12'
+      '10*2'     | '20'
+      '10x2'     | '20'
+      '50-6'     | '44'
+      '-10*2'    | '-20'
+      '-10/2'    | '-10'
+  }
 
   static def evaluate(String expression) {
     return new ExhaustiveCalculator().evaluate(expression)

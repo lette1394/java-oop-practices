29495787234e6e03e6ff20a6c35f310f8fd13ae6
lette1394 <lette1394@gmail.com>
2021-01-04 22:48:13 +0900

    Add RealNumericPatternFinder / PatternFinderFactory

---
+++ calculator3/src/main/java/com/github/lette1394/calculator3/pattern/PatternFinderFactory.java
@@ -0,0 +1,7 @@
+package com.github.lette1394.calculator3.pattern;
+
+final public class PatternFinderFactory {
+  public static RealNumericPatternFinder realNumber() {
+    return new RealNumericPatternFinder();
+  }
+}

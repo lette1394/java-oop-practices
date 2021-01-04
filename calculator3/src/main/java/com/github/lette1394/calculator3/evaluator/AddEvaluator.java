5def911b688c7a4e3a6ef9c67a0328826e50f290
lette1394 <lette1394@gmail.com>
2021-01-04 22:53:13 +0900

    [WIP] Add todos

--- calculator3/src/main/java/com/github/lette1394/calculator3/evaluator/AddEvaluator.java
+++ calculator3/src/main/java/com/github/lette1394/calculator3/evaluator/AddEvaluator.java
@@ -1,14 +1,19 @@
 package com.github.lette1394.calculator3.evaluator;
 
-import static com.github.lette1394.calculator3.common.Contracts.requires;
 import static java.lang.String.format;
 
-import java.util.regex.Matcher;
+import com.github.lette1394.calculator3.pattern.NotFoundPatternException;
+import com.github.lette1394.calculator3.pattern.RealNumericPatternFinder;
 import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class AddEvaluator implements Evaluator {
+  private final RealNumericPatternFinder patternFinder;
+
+  // TODO: operator를 찾는 finder도 넣어주자
+  //  이름은 single character finder? 정도로
+
   private final static Pattern pattern =
     Pattern.compile("\\s*(-?\\d+\\.?\\d*)\\s*(\\+)\\s*(-?\\d+\\.?\\d*)\\s*");
 
@@ -20,13 +25,19 @@ public class AddEvaluator implements Evaluator {
                                                    OverflowException,
                                                    UnderflowException,
                                                    EvaluationTimeoutException {
-    final Matcher matcher = pattern.matcher(expression);
-    requires(matcher.matches(),
-      new UnsupportedExpressionException(format("Not supported expression: %s", expression)));
+    final String left = find(expression);
+    // TODO: 여기에 substring을 계산하는 로직을 넣자.
 
-    final String left = matcher.group(1);
-    final String right = matcher.group(3);
+    final String right = find(expression.substring());
     return adder.add(left, right);
+  }
+
+  private String find(String expression) {
+    try {
+      return patternFinder.find(expression);
+    } catch (NotFoundPatternException e) {
+      throw new UnsupportedExpressionException(format("Not supported expression: %s", expression), e);
+    }
   }
 
   @Override

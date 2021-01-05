f8e1930ba1c5d358a2ac829bcf6b18228ee34c17
lette1394 <lette1394@gmail.com>
2021-01-05 21:01:43 +0900

    Refactor: extract base method

--- calculator3/src/main/java/com/github/lette1394/calculator3/evaluator/AddEvaluator.java
+++ calculator3/src/main/java/com/github/lette1394/calculator3/evaluator/AddEvaluator.java
@@ -1,15 +1,14 @@
 package com.github.lette1394.calculator3.evaluator;
 
-import static java.lang.String.format;
+import com.github.lette1394.calculator3.pattern.PatternFinder;
 
-import com.github.lette1394.calculator3.pattern.NotFoundPatternException;
-import com.github.lette1394.calculator3.pattern.RealNumericPatternFinder;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public class AddEvaluator extends BaseEvaluator {
-  private final RealNumericPatternFinder patternFinder;
   private final Adder adder;
+
+  public AddEvaluator(PatternFinder patternFinder, Adder adder) {
+    super(patternFinder);
+    this.adder = adder;
+  }
 
   @Override
   public String evaluate(String expression) throws UnsupportedExpressionException,
@@ -20,14 +19,6 @@ public class AddEvaluator extends BaseEvaluator {
     final String left = find(expression);
     final String right = find(substringAfter(expression, left));
     return adder.add(left, right);
-  }
-
-  private String find(String expression) {
-    try {
-      return patternFinder.find(expression);
-    } catch (NotFoundPatternException e) {
-      throw new UnsupportedExpressionException(format("Not supported expression: %s", expression), e);
-    }
   }
 
   @Override
--- calculator3/src/main/java/com/github/lette1394/calculator3/evaluator/BaseEvaluator.java
+++ calculator3/src/main/java/com/github/lette1394/calculator3/evaluator/BaseEvaluator.java
@@ -1,7 +1,26 @@
 package com.github.lette1394.calculator3.evaluator;
 
+import static java.lang.String.format;
+
+import com.github.lette1394.calculator3.pattern.NotFoundPatternException;
+import com.github.lette1394.calculator3.pattern.PatternFinder;
+
 public abstract class BaseEvaluator implements Evaluator {
   private static final int INDEX_NOT_FOUND = -1;
+
+  private final PatternFinder patternFinder;
+
+  protected BaseEvaluator(PatternFinder patternFinder) {
+    this.patternFinder = patternFinder;
+  }
+
+  protected final String find(String expression) {
+    try {
+      return patternFinder.find(expression);
+    } catch (NotFoundPatternException e) {
+      throw new UnsupportedExpressionException(format("Not supported expression: %s", expression), e);
+    }
+  }
 
   protected final String substringAfter(String string, String separator) {
     if (string == null) {

d70a2bf27ecea8ed3eb308c5e99722ba97a0df40
lette1394 <lette1394@gmail.com>
2021-01-06 21:21:37 +0900

    Pass all test

--- calculator3/src/test/groovy/com/github/lette1394/calculator3/pattern/IntegerMatchedPatternSpec.groovy
+++ calculator3/src/test/groovy/com/github/lette1394/calculator3/pattern/IntegerMatchedPatternSpec.groovy
@@ -6,14 +6,24 @@ class IntegerMatchedPatternSpec extends Specification {
   def 'find #targets in #expression'() {
     expect:
       def pattern = subject(expression)
-      pattern.next() == '10'
-      pattern.next() == '2'
+      findAll(pattern) == targets
     where:
-      expression = '10+2'
-      targets = ['10', '2']
+      expression   | targets
+      '1'          | ['1']
+      '10'         | ['10']
+      '10+2'       | ['10', '2']
+      '10abc123'   | ['10', '123']
+      '10abc 123'  | ['10', '123']
+      '10 abc123'  | ['10', '123']
+      '10 abc 123' | ['10', '123']
+
+      '2.7'        | ['2', '7']
+      '2.0'        | ['2', '0']
+      '2e+8'       | ['2', '8']
+      '2.0e+8'     | ['2', '0', '8']
   }
 
-  def 'throw exception when expression does not contain integer'() {
+  def 'throw exception in #expression'() {
     when:
       subject(expression).next()
     then:
@@ -23,10 +33,6 @@ class IntegerMatchedPatternSpec extends Specification {
         '       ',
         'abc',
         'not a number',
-        '2.7',
-        '2.0',
-        '2e+8',
-        '2.0e+8'
       ]
   }
 
@@ -42,6 +48,18 @@ class IntegerMatchedPatternSpec extends Specification {
     where:
       expression = '10+2'
       targets = ['10', '2']
+  }
+
+  private static def findAll(MatchedPattern matchedPattern) {
+    def ret = []
+    while (true) {
+      try {
+        ret << matchedPattern.next()
+      } catch (NotMatchedAnymoreException ignored) {
+        break
+      }
+    }
+    return ret
   }
 
   private static MatchedPattern subject(String expression) {
